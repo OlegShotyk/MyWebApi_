b9e4b3f7fbe97d76cfe7eb242a362daea03edc48
Oleg Shotyk <olegshotyk@gmail.com>
2024-05-10 21:13:03 +0300

    Add project files.

---
+++ MyWebApi_/Controllers/GetRandNumberController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyWebApi_.Controllers
+{
+    [Route("api/GetRandNumber")]
+    [ApiController]
+    public class GetRandNumberController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<int>> GetRandomNumber()
+        {
+            return Random.Shared.Next(0, 100);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<int>> PostRandomNumber(int minValue, int maxValue)
+        {
+            if(minValue > maxValue)
+            {
+                return Random.Shared.Next(maxValue, minValue);
+            }
+            return Random.Shared.Next(minValue, maxValue);
+        }
+    }
+}
